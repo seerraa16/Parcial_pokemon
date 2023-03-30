5fbab790c3f303dd0360008c1839020d1629bea9
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-30 11:43:00 +0200

    inicio

--- Ejercicio1/ejercicio1.py
+++ Ejercicio1/ejercicio1.py
@@ -1,3 +1,4 @@
+import random 
 class pokemon:
     def __init__(self, Id, nombre, tipoArma, salud, ataque, defensa):
         self.Id = Id
@@ -6,25 +7,12 @@ class pokemon:
         self.salud = salud
         self.ataque = ataque
         self.defensa = defensa
-    def salud():
-        salud = int(input("Ingrese la salud del pokemon: "))
-        if 1 <= salud <= 100:
-            return salud
-        else:
-            print("Ingrese un valor entre 1 y 100")
-    def ataque():
-        ataque = int(input("Ingrese el ataque del pokemon: "))
-        if 1 <= ataque <= 10:
-            return ataque
-    def defensa(): 
-        defensa = int(input("Ingrese la defensa del pokemon: "))
-        if 1 <= defensa <= 10:
-            return defensa
-    def tipoArma():
-        tipoArma = ["Puñetazo", "Patada", "Codazo", "Cabezazo"]
-        print ("Elige el tipo de arma: ")
-        arma = int(input("1. Puñetazo, 2. Patada, 3. Codazo, 4. Cabezazo: "))
-        if 1 <= arma <= 4:
-            return tipoArma[arma-1]
-        else:
-            print("Ingrese un valor entre 1 y 4")
+
+        
+        if not 1 <= salud <= 100:
+            raise ValueError("Health points must be between 1 and 100")
+        if not 1 <= ataque <= 10:
+            raise ValueError("Attack index must be between 1 and 10")
+        if not 1 <= defensa <= 10:
+            raise ValueError("Defense index must be between 1 and 10")            
+    
