4a124ecb22b0edec4f9ce1a9d9750261944cc151
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-30 19:16:18 +0200

    pasos anteriores

--- pokemon_air.py
+++ pokemon_air.py
@@ -70,12 +70,12 @@ class PokemonAir(Pokemon):
       >>> from weapon_type import WeaponType
       >>> obj_Pokemon = PokemonEarth(1, "Pidgey", WeaponType.PUNCH, 100, 7, 10)
     """
-    def __init__(self, Id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating, fight_defense):
-        super().__init__(Id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating,fight_defense)
+    def __init__(self, Id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating):
+        super().__init__(Id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating)
     def fight_defense(self, damage):
         azar = random.randint(1, 2)
         if azar == 1:
-            self._health_points = self._health_points - damage
+            self._health_points -= damage
             return True
         else:
             return False
