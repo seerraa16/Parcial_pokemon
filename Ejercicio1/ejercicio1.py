935974f08bc6b4f05f1ba87746bad719773e1ab3
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-30 11:50:27 +0200

    escoger valor

--- Ejercicio1/ejercicio1.py
+++ Ejercicio1/ejercicio1.py
@@ -10,11 +10,17 @@ class pokemon:
 
         
         if not 1 <= salud <= 100:
-            print("El valor de la salud deve ser entre 1 y 100")
+            print("El valor de la salud deve ser entre 1 y 100, escoga otro valor")
+            saludnueva = int(input("Ingrese el valor de la salud: "))
+            
         if not 1 <= ataque <= 10:
-            print("El ataque debe estar entre 1 y 10")
+            print("El ataque debe estar entre 1 y 10. Escoga otro valor")
+            ataquenuevo = int(input("Ingrese el valor del ataque: "))
         if not 1 <= defensa <= 10:
-            print("La defensa debe estar entre 1 y 10")
+            print("La defensa debe estar entre 1 y 10, escoga otro valor")
+            defensanueva = int(input("Ingrese el valor de la defensa: "))
         if not Id == int:
-            print("El Id debe ser un numero entero")
+            print("El Id debe ser un numero entero, escoga otro valor")
+            Idnuevo = int(input("Ingrese el valor del Id: "))
+            
     
