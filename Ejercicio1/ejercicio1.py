037311e72865a16fdc939ec2b6c95e1c871781c1
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-30 10:45:17 +0200

    clase

---
+++ Ejercicio1/ejercicio1.py
@@ -0,0 +1,8 @@
+class pokemon:
+    def __init__(self, Id, nombre, tipoArma, salud, ataque, defensa):
+        self.Id = Id
+        self.nombre = nombre
+        self.tipoArma = tipoArma
+        self.salud = salud
+        self.ataque = ataque
+        self.defensa = defensa
